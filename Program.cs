78b55a1b3753f3189d73662a56f5ffe34e79d9b8
ct1kep <114058427+ct1kep@users.noreply.github.com>
2022-09-23 17:50:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите сумму Вашего месячного дохода в гривнах (используя числовой формат записи)");
+string profit = Console.ReadLine();
+decimal profitInDecimal = Convert.ToDecimal(profit);
+decimal singleTaxPercentage = 0.05m;
+decimal singleTax = profitInDecimal * singleTaxPercentage;
+decimal singleDeposit;
+decimal singleDepositPercentage = 0.22m;
+// с помощью оператора if  сравниваю доход пользователя с 6500, для корректного вычисления singleDeposit.
+if (profitInDecimal <= 6500)
+{
+    singleDeposit = profitInDecimal * singleDepositPercentage;
+}
+else
+{
+    singleDeposit = 6500 * singleDepositPercentage;
+}
+decimal profitAfterTaxes = profitInDecimal - singleTax - singleDeposit;
+
+Console.WriteLine("Сумма вашего дохода составляет " + profitInDecimal + "грн");
+Console.WriteLine("Единый налог составит " + singleTax + "грн");
+Console.WriteLine("Единый социальный вклад составит " + singleDeposit + "грн");
+Console.WriteLine("Ваш доход за вычетом налогов составит " + profitAfterTaxes + "грн");
+
+Console.ReadKey();
+
