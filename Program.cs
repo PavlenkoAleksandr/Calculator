42059c11b690224de2a949f3ca8759f0b5ad78d1
ct1kep <sashapavlenko1996@gmail.com>
2022-09-25 22:51:37 +0300

    added variables for String.Format

--- Program.cs
+++ Program.cs
@@ -12,46 +12,46 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            decimal singleTaxPercentage = 0.05m;
-            decimal singleDepositPercentage = 0.22m;
+            decimal singleTaxRate = 0.05m;
+            decimal singleDepositRate = 0.22m;
             int minProfit = 6500;
-            string profit; 
-            decimal profitInDecimal;
+            string income; 
+            decimal incomeInDecimal;
             decimal singleTax;
             decimal singleDeposit;
-            decimal profitAfterTaxes;
+            decimal profit;
             decimal exchangeUSD = 37.17m;
             decimal exchangeEUR = 36.01m;
             string currencies;
-            decimal profitInDecAfterExchange;
+            decimal incomeAfterExchange;
+            string incomeInUahForUser;
+            string singleTaxForUser;
+            string singleDepositForUser;
+            string profitForUser;
 
             Console.WriteLine("Приветствую Вас в калькуляторе доходов!");
-
 
             Beggining:
             Console.WriteLine("Введите, пожалуйста, валюту в которой получаете доход\nUSD - в долларах, EUR - в евро, UAH - в гривне");
             currencies = Console.ReadLine();
 
-
             Console.WriteLine("Введите сумму Вашего месячного дохода в валюте, которую указали выше(используя числовой формат записи)");
-            profit = Console.ReadLine();
-            profitInDecimal = Convert.ToDecimal(profit);
-
+            income = Console.ReadLine();
+            incomeInDecimal = Convert.ToDecimal(income);
 
             //сравниваю введённую пользователем валюту, если допустил ошибку - возвращаю на этап ввода валюты
 
-
             if (currencies.Equals("EUR"))
             {
-                profitInDecAfterExchange = profitInDecimal * exchangeEUR;
+                incomeAfterExchange = incomeInDecimal * exchangeEUR;
             }
             else if (currencies.Equals("USD"))
             {
-                profitInDecAfterExchange = profitInDecimal * exchangeUSD;
+                incomeAfterExchange = incomeInDecimal * exchangeUSD;
             }
             else if (currencies.Equals("UAH"))
             {
-                profitInDecAfterExchange = profitInDecimal;
+                incomeAfterExchange = incomeInDecimal;
             }
             else
             {
@@ -59,20 +59,20 @@ namespace ConsoleApp1
                 goto Beggining;
             }
            
-
-            singleTax = profitInDecAfterExchange * singleTaxPercentage;
-            singleDeposit = minProfit * singleDepositPercentage;
-            profitAfterTaxes = profitInDecAfterExchange - singleTax - singleDeposit;
-
+            singleTax = incomeAfterExchange * singleTaxRate;
+            singleDeposit = minProfit * singleDepositRate;
+            profit = incomeAfterExchange - singleTax - singleDeposit;
 
             //у меня начали появляться лишние цифры после запятой, нагуглил спецификатор вывода и использовал его
+            incomeInUahForUser = String.Format("{0:f2}",incomeAfterExchange);
+            singleTaxForUser = String.Format("{0:f2}", singleTax);
+            singleDepositForUser = String.Format("{0:f2}",singleDeposit);
+            profitForUser = String.Format("{0:f2}",profit);
 
-
-            Console.WriteLine("Сумма вашего дохода составляет " + String.Format("{0:f2}",profitInDecAfterExchange) + "грн");
-            Console.WriteLine("Единый налог составит " + String.Format("{0:f2}", singleTax) + "грн");
-            Console.WriteLine("Единый социальный вклад составит " + String.Format("{0:f2}",singleDeposit) + "грн");
-            Console.WriteLine("Ваш доход за вычетом налогов составит " + String.Format("{0:f2}",profitAfterTaxes) + "грн");
-
+            Console.WriteLine("Сумма вашего дохода составляет " + incomeInUahForUser + "грн");
+            Console.WriteLine("Единый налог составит " + singleTaxForUser + "грн");
+            Console.WriteLine("Единый социальный вклад составит " + singleDepositForUser + "грн");
+            Console.WriteLine("Ваш доход за вычетом налогов составит " + profitForUser + "грн");
 
             Console.ReadKey();
 
