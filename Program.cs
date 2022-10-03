26100d131fcd24a7e0b5451a5425b9f02d800fda
ct1kep <sashapavlenko1996@gmail.com>
2022-10-03 19:18:35 +0300

    made separate methods for ShowBeginning, Calculation and ShowResult

--- Program.cs
+++ Program.cs
@@ -10,16 +10,8 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        
-
         static void Main(string[] args)
-        {
-            //здесь почему-то ругается, если делаю его private
-            private string FormattoString(decimal value)
-            {
-                return String.Format("{0:f2}", value);
-            }
-
+        { 
             decimal singleTaxRate = 0.05m;
             decimal singleDepositRate = 0.22m;
             int minProfit = 6500;
@@ -34,17 +26,25 @@ namespace ConsoleApp1
             decimal incomeAfterExchange = 0;
 
             Console.WriteLine("Приветствую Вас в калькуляторе доходов!");
-            Console.WriteLine("Введите, пожалуйста, валюту в которой получаете доход\nUSD - в долларах, EUR - в евро, UAH - в гривне");
-            currencies = Console.ReadLine();
+            ShowBeginning();
+            Calculation();
 
-            Console.WriteLine("Введите сумму Вашего месячного дохода в валюте, которую указали выше(используя числовой формат записи)");
-            income = Console.ReadLine();
-            incomeInDecimal = Convert.ToDecimal(income);
 
-            //сравниваю введённую пользователем валюту c тремя возможными вариантами
 
-            switch (currencies)
+            void ShowBeginning()
             {
+                Console.WriteLine("Введите, пожалуйста, валюту в которой получаете доход\nUSD - в долларах, EUR - в евро, UAH - в гривне");
+                currencies = Console.ReadLine();
+
+                Console.WriteLine("Введите сумму Вашего месячного дохода в валюте, которую указали выше(используя числовой формат записи)");
+                income = Console.ReadLine();
+                incomeInDecimal = Convert.ToDecimal(income);
+            }
+          
+            void Calculation()
+            {
+                switch (currencies)
+                {
                 case "EUR":
                     incomeAfterExchange = incomeInDecimal * exchangeEUR;
                     break;
@@ -54,25 +54,38 @@ namespace ConsoleApp1
                 case "UAH":
                     incomeAfterExchange = incomeInDecimal;
                     break;
+                }
+
+                singleTax = incomeAfterExchange * singleTaxRate;
+                singleDeposit = minProfit * singleDepositRate;
+                profit = incomeAfterExchange - singleTax - singleDeposit;
+
+                if (incomeAfterExchange == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Некорректный ввод валюты, повторите ещё раз. Обратите внимание на регистр и язык ввода");
+                    ShowBeginning();
+                    Calculation();
+                }
+                else
+                {
+                    ShowResult();
+                }
             }
 
-            singleTax = incomeAfterExchange * singleTaxRate;
-            singleDeposit = minProfit * singleDepositRate;
-            profit = incomeAfterExchange - singleTax - singleDeposit;
-
-            if (incomeAfterExchange == 0)
+            void ShowResult()
             {
-                Console.WriteLine("Некорректный ввод данных!");
+                Console.WriteLine("Сумма вашего дохода составляет " + FormattoString(incomeAfterExchange) + "грн");
+                Console.WriteLine("Единый налог составит " + FormattoString(singleTax) + "грн");
+                Console.WriteLine("Единый социальный вклад составит " + FormattoString(singleDeposit) + "грн");
+                Console.WriteLine("Ваш доход за вычетом налогов составит " + FormattoString(profit) + "грн");
+                Console.ReadKey();
             }
-            else
-            {
-            Console.WriteLine("Сумма вашего дохода составляет " + FormattoString(incomeAfterExchange) + "грн");
-            Console.WriteLine("Единый налог составит " + FormattoString(singleTax) + "грн");
-            Console.WriteLine("Единый социальный вклад составит " + FormattoString(singleDeposit) + "грн");
-            Console.WriteLine("Ваш доход за вычетом налогов составит " + FormattoString(profit) + "грн");
-            }
-            Console.ReadKey();
 
+            string FormattoString(decimal value)
+            {
+                return String.Format("{0:f2}", value);
+            }
         }
     }
 }
